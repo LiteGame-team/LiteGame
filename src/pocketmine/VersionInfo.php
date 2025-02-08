ad7c3e14560bcb02457b0fc6f97905b1839fd6ba
ShadowUnix <168187778+Rconov@users.noreply.github.com>
2025-02-08 22:45:38 +1100

    .

---
+++ src/pocketmine/VersionInfo.php
@@ -0,0 +1,14 @@
+<?php
+namespace pocketmine;
+use pocketmine\PocketMine;
+use pocketmine\Server;
+class VersionInfo {
+    public const NAME = "LiteGame";
+    public const VERSION = "1.1.X";
+    public const API_VERSION = "3.0.1";
+    public const CODENAME = "Pozor";
+    public const GENISYS_API_VERSION = '2.0.0';
+    public const CORE_VERSION = '0.0.1-BETA';
+    public const MIN_PHP_VERSION = "8.0.0";
+}
+?>
